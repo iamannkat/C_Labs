4feb9abf4e6a6b83ca248335e397401af3648a12
Anna S <31012236+iamannkat@users.noreply.github.com>
2019-12-27 17:04:25 +0200

    Create lab8.1.c

---
+++ lab8/lab8.1.c
@@ -0,0 +1,60 @@
+// Endeiktiki Lysi tis Askisis 8.1 - Ak. Etos 2019-2020
+
+#include <stdio.h>      /* For scanf, printf, puts */
+#include <stdlib.h>     /* For exit */
+#include <unistd.h>     /* For fork, execl */
+#include <errno.h>      /* For perror */
+#include <sys/wait.h>   /* For waitpid */
+
+#define NUM 7
+
+typedef struct {     /* Nice structure */
+	char *name;
+	char *menutitle;
+	char *fullpath;
+} option_t;
+
+
+/* Array of structures */
+option_t menu[NUM] = {
+  { NULL,               "0. exit",                      NULL },
+  { "firefox",          "1. run firefox",               "/usr/bin/firefox" },
+  { "firefox",          "2. run firefox (and wait)",    "/usr/bin/firefox" },
+  { "gedit",            "3. run gedit",                 "/usr/bin/gedit" },
+  { "gedit",            "4. run gedit (and wait)",      "/usr/bin/gedit" },
+  { "gnome-calculator", "5. run gnome-calc",            "/usr/bin/gnome-calculator" },
+  { "gnome-calculator", "6. run gnome-calc (and wait)", "/usr/bin/gnome-calculator" }
+};
+
+
+int main(void)
+{
+	int i, sel, pid;
+
+	while (1)   /* loop for ever */
+	{
+		for (i = 0; i < NUM; i++)
+			puts(menu[i].menutitle);
+		printf("\nPlease enter your selection: ");
+
+		scanf("%d", &sel);
+		if (sel < 0 || sel >= NUM)
+			continue;
+		if (sel == 0)
+			break;
+
+		if ((pid = fork()) == 0)   /* Create child process */
+		{
+			/* Exec application */
+			execl(menu[sel].fullpath, menu[sel].name, NULL);
+			perror("execl()");   /* This should not be executed */
+			exit(1);
+		}
+		else    /* Me (parent) */
+		{
+			if ((sel & 1) == 0)   /* Even choice */
+				waitpid(pid, NULL, 0);
+		}
+	}
+	return 0;
+}
